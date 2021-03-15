84cafa70719bf4c530af21babccd0f2655a0cf7e
jmatstetson <jmathison@stetson.edu>
2021-03-14 21:36:33 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hey man'));
+app.get('/', (req, res) => res.send('joey was here'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
